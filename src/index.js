d71e418693a71354ea48a0e79daedd5f61101a08
Neejor <neejorchakma@gmail.com>
2021-02-02 21:15:23 +0530

    plx

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import store from "./store/store";
 
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 // const myStore = createStore(allReducers,
